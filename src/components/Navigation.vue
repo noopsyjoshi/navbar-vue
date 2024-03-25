03c045ba01097f80ec05f92af94817f609bacc2a
noopsyjoshi <noopur.joshi13@gmail.com>
2024-03-25 10:44:24 +0000

    Updated JSON file, collapsable secondary nav

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -20,36 +20,79 @@
       </div>
 
       <div class="navigation__menu" :class="this.showMobileMenu ? 'open' : ''">
-        <!-- Primary nav - Platform, Resources, About us  -->
         <ul class="navigation__primary">
           <li
             class="navigation__item"
             v-for="primaryItem in navPrimaryItems"
             :key="primaryItem.text"
           >
-            <!-- If nav item has submenu, display a button to expand submenu -->
             <a
               v-if="primaryItem.navSecondaryItems"
               aria-haspopup="true"
               aria-expanded="false"
               :aria-controls="primaryItem.text"
-              @click.prevent="event.preventDefault()"
-              :href="primaryItem.link"
+              :class="this.showSecondaryNav ? 'is-active' : ''"
+              @click.prevent="toggleSecondaryNav()"
+              href="/"
               class="navigation__link"
             >
               {{ primaryItem.text }}
             </a>
-            <!-- Else display link -->
-            <router-link
-              v-else
-              to="primaryItem.link"
-              :target="primaryItem.target"
-              class="navigation__link"
-            >
+
+            <router-link v-else to="" class="navigation__link">
               {{ primaryItem.text }}
             </router-link>
+
+            <div :class="this.showSecondaryNav ? 'is-active' : ''" class="navigation__secondary">
+              <ul>
+                <li
+                  class="navigation__secondary-item"
+                  v-for="(secondaryItem, index) in primaryItem.navSecondaryItems"
+                  :key="secondaryItem.text"
+                >
+                  <div class="navigation-collapse" :class="{ active: activeIndex === index }">
+                    <div class="navigation-collapse__header" @click="toggleAccordion(index)">
+                      <Arrow class="navigation-collapse__arrow" />
+                      <h3 class="navigation-collapse__title">{{ secondaryItem.text }}</h3>
+                    </div>
+                    <div
+                      v-show="activeIndex === index"
+                      v-if="secondaryItem.navContent"
+                      class="navigation-collapse__content"
+                    >
+                      <div v-for="content in secondaryItem.navContent" :key="content.text">
+                        <h3>{{ content.title }}</h3>
+                        <p>{{ content.body }}</p>
+                        <div
+                          class="navigation-collapse__link"
+                          v-for="link in content.links"
+                          :key="link.text"
+                        >
+                          <router-link to="link.url">
+                            <img
+                              class="navigation-collapse__icon"
+                              :src="link.icon.url"
+                              :alt="link.icon.alt"
+                            />
+                            <span>{{ link.text }}</span>
+                            <Open class="navigation-collapse__open-icon" />
+                          </router-link>
+                        </div>
+                        <div class="navigation-collapse__capabilities">
+                          <h4>Platform Capabilities</h4>
+                          <span v-for="capability in content.capabilities" :key="capability.text">
+                            {{ capability.text }}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </li>
         </ul>
+
         <router-link to="/" type="button" class="button navigation__button">
           Book a demo
         </router-link>
@@ -62,19 +105,26 @@
 import Logo from '@/assets/icons/logo.svg'
 import Hamburger from '@/assets/icons/hamburger.svg'
 import Close from '@/assets/icons/close.svg'
-import navigationData from '../../public/data/navigation.json'
+import Arrow from '@/assets/icons/arrow.svg'
+import Open from '@/assets/icons/open.svg'
+import navigationData from '/public/data/navigation.json'
 
 export default {
   components: {
     Logo,
     Hamburger,
-    Close
+    Close,
+    Arrow,
+    Open
   },
 
   data() {
     return {
       showMobileMenu: false,
-      navPrimaryItems: navigationData.navPrimaryItems
+      navPrimaryItems: navigationData.navPrimaryItems,
+      showSecondaryNav: false,
+      expandedMenuSection: null,
+      activeIndex: 0
     }
   },
 
@@ -90,6 +140,16 @@ export default {
       this.showMobileMenu === true
         ? document.querySelector('body').classList.add('overflow')
         : document.querySelector('body').classList.remove('overflow')
+    },
+
+    toggleSecondaryNav() {
+      this.showSecondaryNav = !this.showSecondaryNav
+    },
+
+    toggleAccordion(index) {
+      console.log(this.activeIndex, index)
+
+      this.activeIndex = this.activeIndex === index ? -1 : index
     }
   }
 }
